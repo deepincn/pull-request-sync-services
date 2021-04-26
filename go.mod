52df16142c590cd763cbadec8a69cf9c532feff8
justforlxz <justforlxz@gmail.com>
2021-04-26 10:14:29 +0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/colorful-fullstack/PRTools
+
+go 1.16
+
+require github.com/google/go-github/v35 v35.1.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/google/go-github/v35/github"
+
+func main() {
+	http.HandleFunc(${1:pattern string}, ${2:handler func(ResponseWriter, *Request))
+
+	client := github.NewClient()
+
+}
